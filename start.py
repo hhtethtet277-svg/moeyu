0a1480731288a71e0444be0fc603c784cf84001a
hhtethtet277-svg <hhtethtet277@gmail.com>
2026-05-10 11:23:53 +0630

    Update and rename run.py to start.py

---
+++ start.py
@@ -0,0 +1,10 @@
+import hack
+import sys
+
+if __name__ == "__main__":
+    try:
+        # hack.py ထဲက logic တွေက function ထဲမှာဆိုရင် ဒီမှာ လှမ်းခေါ်ပါ
+        # ဥပမာ - hack.main()
+        pass
+    except Exception as e:
+        print(f"Error: {e}")
