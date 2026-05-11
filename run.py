fe36904ff628c618a629f63a35031fdd0f1477b9
hhtethtet277-svg <hhtethtet277@gmail.com>
2026-05-11 20:13:44 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,16 @@
-import hack # hhh.py ကို hack လို့ နာမည်ပေးထားရင်
+import hack # သင့်ရဲ့ .so ဖိုင်နာမည်
 import sys
 
 def main():
     # ၁။ License ကို အရင်ဆုံး စစ်ဆေးမယ်
     if hack.check_license():
-        # ၂။ License မှန်မှသာ ကျန်တဲ့ Setup အပိုင်းတွေကို လုပ်ဆောင်မယ်
+        # ၂။ Key မှန်မှသာ Setup အပိုင်းတွေကို လုပ်ဆောင်မယ်
         print("\n\033[1;32m[+] Setting up the wifi info...")
         print("[+] Unbinding wifi success")
-        print("[+] Trying to get info")
         print("[+] Setup success")
         
-        # သင့်ရဲ့ main feature ကို ဒီမှာ ခေါ်သုံးပါ
-        # hack.feature() 
+        # သင့်ရဲ့ main function ကို ဒီအောက်မှာ ထည့်ပါ
+        # hack.start_menu()
 
 if __name__ == "__main__":
     main()
