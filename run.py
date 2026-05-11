112df451fcd45bf614bc70766d5a431d293c3e73
hhtethtet277-svg <hhtethtet277@gmail.com>
2026-05-11 13:01:46 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import hack
 import sys
 
-if __name__ == "__main__":
+def main():
     try:
+        # hack.so ထဲက feature function ကို လှမ်းခေါ်တာပါ
         hack.feature()
     except KeyboardInterrupt:
         print("\n[!] အသုံးပြုသူမှ ရပ်တန့်လိုက်ပါသည်။")
         sys.exit()
     except Exception as e:
         print(f"\n[!] Error ဖြစ်ပွားပါသည်: {e}")
+
+if __name__ == "__main__":
+    main()
