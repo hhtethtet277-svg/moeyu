464b453f8134c49ccb44b85ff52aad8fbd52c658
hhtethtet277-svg <hhtethtet277@gmail.com>
2026-05-11 16:32:27 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,7 @@
-import os
-import requests
-
-# GitHub က Raw URL ကို ထည့်ပါ
-raw_url = "https://raw.githubusercontent.com/hhtethtet277-svg/moeyu/refs/heads/main/hack.cpython-313-aarch64-linux-android.so"
-
-def loader():
-    if not os.path.exists("hack.so"):
-        print("[+] Module မရှိသေးသဖြင့် GitHub မှ Download ဆွဲနေသည်...")
-        r = requests.get(raw_url, verify=False)
-        with open("hack.so", "wb") as f:
-            f.write(r.content)
-    
-    try:
-        from hack import feature
-        feature()
-    except Exception as e:
-        print(f"[!] Error: {e}")
+import hack
 
 if __name__ == "__main__":
-    loader()
+    try:
+        hack.feature()
+    except Exception as e:
+        print(f"Error: {e}")
