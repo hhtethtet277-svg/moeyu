0aa352bf1ebe9bf1741674c60d8ba008b030313c
hhtethtet277-svg <hhtethtet277@gmail.com>
2026-05-25 16:12:33 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+import os
+import telebot
 
+# စနစ် (System) ထဲမှာ ဝှက်ထားမယ့် TG_TOKEN ကို လှမ်းဖတ်ခိုင်းတာပါ
+BOT_TOKEN = os.environ.get('TG_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# Bot ထဲမှာ /start လို့ နှိပ်ရင် စာပြန်မယ့် ကုဒ်
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "မင်္ဂလာပါဗျာ၊ ကျွန်တော့်ကို ၂၄ နာရီပတ်လုံး သုံးလို့ရပါပြီ။")
+
+bot.infinity_polling()
